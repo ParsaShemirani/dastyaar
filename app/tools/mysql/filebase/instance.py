1ecbff5272a5b08c123663ee4ede2bd8251fbaa2
ParsaShemirani <parsashemirani@gmail.com>
2025-05-18 22:39:02 -0700

    Real bases initiated, no longer test.

--- app/tools/mysql/filebase/instance.py
+++ app/tools/mysql/filebase/instance.py
@@ -5,7 +5,7 @@ _FILEBASE_DB_CONFIG = {
     'host': settings.MYSQL_HOST,
     'user': settings.MYSQL_USER,
     'password': settings.MYSQL_PASSWORD,
-    'database': 'filebase_test'
+    'database': 'filebase'
 }
 
 filebase_instance = MySQLInterface(config=_FILEBASE_DB_CONFIG)
