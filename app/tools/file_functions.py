85817f7257f44667bf51bc698465c41ca7ed4dbf
ParsaShemirani <parsashemirani@gmail.com>
2025-06-09 06:42:17 -0700

    merge genrate new filename and genereate first filename into one

--- app/tools/file_functions.py
+++ app/tools/file_functions.py
@@ -57,19 +57,17 @@ def copy_file(file_path,dst_dir):
 
 def generate_new_filename(file_path, version_number, hash):
     hex_hash = hash.hex()
+    if version_number == 1:
+        basename = os.path.basename(file_path)
+        filename, ext= os.path.splitext(basename)
+        result = f"{filename}-v{version_number}-{hex_hash}{ext}"
+        return result
     pattern = r'^(.+)-v(\d+)-([0-9A-Fa-f]+)(\.[^.]+)$'
     filename = os.path.basename(file_path)
     m = re.match(pattern, filename)
     basename = m.groups(1)
     ext = m.groups(4)
     result = f"{basename}-v{version_number}-{hex_hash}{ext}"
-    return result
-
-def generate_first_filename(file_path, version_number, hash):
-    hex_hash = hash.hex()
-    basename = os.path.basename(file_path)
-    filename, ext= os.path.splitext(basename)
-    result = f"{filename}-v{version_number}-{hex_hash}{ext}"
     return result
 
 def extract_voice_rec_ts(file_path):    
