e5c97f94c6631621aa3dea5cac7b177d7a19d0bd
ParsaShemirani <parsashemirani@gmail.com>
2025-06-14 21:45:01 -0700

    flask setup nice

--- app/tools/flask_sqliteinterface.py
+++ app/tools/flask_sqliteinterface.py
@@ -4,14 +4,18 @@ from app.tools.settings import FILEBASE_DB_FILE
 
 interface = SQLiteInterface(database_path=FILEBASE_DB_FILE)
 
-def dict_bin_to_hex(dictionary):
-    out = {}
-    for k, v in dictionary.items():
-        if isinstance(v, bytes):
-            out[k] = v.hex()
-        else:
-            out[k] = v
-    return out
+def bin_to_hex(obj):
+    def convert(d):
+        result = {}
+        for k, v in d.items():
+            result[k] = v.hex() if isinstance(v, (bytes, bytearray)) else v
+        return result
+
+    if isinstance(obj, dict):
+        return convert(obj)
+    if isinstance(obj, list):
+        return [convert(d) for d in obj]
+
 
 
 def create_app():
@@ -22,22 +26,17 @@ def create_app():
         parameter_dict = {}
         parameter_dict['query'] = request.args.get('query')
         parameter_dict['params'] = request.args.get('params')
-        parameter_dict['fetch_one'] = request.args.get('fetch_one')
+        fetch_one_arg = request.args.get('fetch_one')
+        parameter_dict['fetch_one'] = str(fetch_one_arg).lower() == 'true'
 
         result = interface.execute_read(
             **{k: v for k, v in parameter_dict.items() if v is not None}
         )
-        if parameter_dict['fetch_one'] == True:
-            if result is None:
-                return None
-            else:
-                result_dict = dict(result)
+        if result is None:
+            return jsonify(result)
         else:
-            result_dict = dict(result[0])
-        safe_dict = dict_bin_to_hex(
-            dictionary=result_dict
-        )
-        return jsonify(safe_dict)
+            safe_dict = bin_to_hex(result)
+            return jsonify(safe_dict)
 
     @app.route("/execute_write", methods=['POST'])
     def execute_write():
@@ -46,17 +45,14 @@ def create_app():
         parameter_dict = {}
         parameter_dict['query'] = data.get('query')
         parameter_dict['params'] = data.get('params')
-        parameter_dict['many'] = data.get('many')
-        print("PARAM DICT")
-        print(parameter_dict)
-
+        many_arg = data.get('many')
+        parameter_dict['many'] = str(many_arg).lower() == 'true'
 
         result = interface.execute_write(
             **{k: v for k, v in parameter_dict.items() if v is not None}
         )
-        print("RESULT MAN")
-        print(result)
-        return 'Thanks for your support'
+
+        return jsonify({"rows_affected": result})
     return app
 
 """
@@ -68,8 +64,8 @@ app.run(host="0.0.0.0", port=5321)
 """
 import requests
 input = {
-    "query": "SELECT * FROM files ORDER BY id DESC LIMIT 1",
-    "fetch_one": True
+    "query": "SELECT * FROM files ORDER BY id DESC LIMIT 3",
+    "fetch_one": False
 }
 r = requests.get("http://127.0.0.1:5321/execute_read", params=input)
 """
@@ -78,7 +74,7 @@ r = requests.get("http://127.0.0.1:5321/execute_read", params=input)
 """
 import requests
 input = {
-    "query": "SELECT * FROM files WHERE id=12343",
+    "query": "SELECT * FROM files WHERE id=11234",
     "fetch_one": True
 }
 r = requests.get("http://127.0.0.1:5321/execute_read", params=input)
@@ -91,7 +87,8 @@ r = requests.get("http://127.0.0.1:5321/execute_read", params=input)
 import requests
 input = {
     "query": "INSERT INTO files_groupings (file_id, grouping_id) VALUES (?, ?)",
-    "params": (352, 234)
+    "params": [(352, 234), (34543,234)],
+    "many": True
 }
 r = requests.post("http://127.0.0.1:5321/execute_write", json=input)
 """
