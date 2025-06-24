f05587548b72fc64c1f47fb32a0297f79e6a89c0
ParsaShemirani <parsashemirani@gmail.com>
2025-06-23 20:04:35 -0700

    jamieman

--- app/tools/ingestor.py
+++ app/tools/ingestor.py
@@ -68,8 +68,8 @@ def main(file_path, groupings, interactive=True):
 
 def folder_main():
     folder_path = TO_INGEST_PATH
-    groupings = [6]
-    interactive = True
+    groupings = [7]
+    interactive = False
     for i in range(3):
         print("CHECK GROUPINGS\n")
         print(groupings)
