43186532e9468bc1aca82afc65720221b30b31d6
ParsaShemirani <parsashemirani@gmail.com>
2025-06-25 17:58:50 -0700

    update interrface

---
+++ sqliteinterface.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+class SQLiteInterface:
+    def __init__(self, database_path):
+        self.database_path = database_path
+
+    def db_execute_read(self, query, params=[], fetch_one=False):
+        connection = sqlite3.connect(self.database_path)
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute(query, params)
+            if fetch_one:
+                row = cursor.fetchone()
+                return dict(row) if row else None
+            else:
+                rows = cursor.fetchall()
+                return [dict(r) for r in rows]
+        finally:
+            cursor.close()
+            connection.close()
+
+    def db_execute_write(self, query, params=[], many=False):
+        connection = sqlite3.connect(self.database_path)
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+
+        try:
+            if many:
+                cursor.executemany(query, params)
+            else:
+                cursor.execute(query, params)
+            connection.commit()
+            return cursor.rowcount
+        finally:
+            cursor.close()
+            connection.close()
